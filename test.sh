d98680c5138634011187c808577a0536d2031b44
Lars Olesen <lars@intraface.dk>
2012-07-31 08:37:11 +0200

    Reverted to just using drush

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-drush/drush make vih_dk.build --prepare-install .
+drush make vih_dk.build --prepare-install .
 test -d profiles/vih_dk
 test -d profiles/vih_dk/modules/contrib
 test -d profiles/vih_dk/modules/vih
