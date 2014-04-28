005f7d9f19a1ba83d0ab94ed0faf4926a11b5dc9
Lars Olesen <lars@intraface.dk>
2014-04-28 09:27:38 +0000

    Added test for one time login

--- tests/behat/features/bootstrap/FeatureContext.php
+++ tests/behat/features/bootstrap/FeatureContext.php
@@ -7,6 +7,7 @@ use Behat\Behat\Context\ClosuredContextInterface,
 use Behat\Gherkin\Node\PyStringNode,
     Behat\Gherkin\Node\TableNode;
 use Drupal\DrupalExtension\Context\DrupalContext;
+use Drupal\Component\Utility\Random;
 //
 // Require 3rd-party libraries here:
 //
@@ -19,17 +20,49 @@ use Drupal\DrupalExtension\Context\DrupalContext;
  */
 class FeatureContext extends DrupalContext
 {
-    /**
-     * Initializes context.
-     * Every scenario gets it's own context object.
-     *
-     * @param array $parameters context parameters (set them up through behat.yml)
-     */
-    public function __construct(array $parameters)
-    {
-        // Initialize your context here
+  /**
+   * Initializes context.
+   * Every scenario gets it's own context object.
+   *
+   * @param array $parameters context parameters (set them up through behat.yml)
+   */
+  public function __construct(array $parameters)
+  {
+    // Initialize your context here
+  }
+
+  /**
+   * @Given /^I log in with the One Time Login Url$/
+   */
+  public function iLogInWithTheOneTimeLoginUrl() {
+    if ($this->loggedIn()) {
+      $this->logOut();
     }
 
+    // Create user (and project)
+    $user = (object) array(
+      'name' => Random::name(8),
+      'pass' => Random::name(16),
+      'role' => 'authenticated user',
+    );
+    $user->mail = "{$user->name}@example.com";
+
+    // Create a new user.
+    $this->getDriver()->userCreate($user);
+
+    $this->users[$user->name] = $this->user = $user;
+
+    $base_url = rtrim($this->locatePath('/'), '/');
+    $login_link = $this->getMainContext()->getDriver('drush')->drush('uli', array(
+      "'$user->name'",
+      '--browser=0',
+      "--uri=${base_url}",
+    ));
+    // Trim EOL characters. Required or else visiting the link won't work.
+    $login_link = trim($login_link);
+    $this->getSession()->visit($this->locatePath($login_link));
+    return TRUE;
+  }
 //
 // Place your definition and hook methods here:
 //
--- tests/behat/features/login.feature
+++ tests/behat/features/login.feature
@@ -9,3 +9,12 @@ I want to be able to login
     And I fill in "admin" for "edit-pass"
     And I press "Log in"
     Then I should see "Log out"
+
+  @drush
+  Scenario: User can change password after using one-time-login-url
+    Given I log in with the One Time Login Url
+    Then I should see "You have just used your one-time login link. It is no longer necessary to use this link to log in. Please change your password."
+    When I fill in "mycoolpassword" for "Password"
+    And I fill in "mycoolpassword" for "Confirm password"
+    And I press "Save"
+    Then I should see "The changes have been saved."
