e3606fd8c082a05e26e865d51c9b7ea66e8355c1
Lars Olesen <lars@intraface.dk>
2014-06-05 09:50:12 +0000

    Added test for Panels and Panels IPE is working

--- tests/behat/features/bootstrap/FeatureContext.php
+++ tests/behat/features/bootstrap/FeatureContext.php
@@ -28,7 +28,7 @@ class FeatureContext extends DrupalContext
    */
   public function __construct(array $parameters)
   {
-    // Initialize your context here
+    $this->useContext('panels', new PanelsSubContext());
   }
 
   /**
@@ -73,15 +73,47 @@ class FeatureContext extends DrupalContext
     $this->getSession()->visit($this->locatePath($login_link));
     return TRUE;
   }
-//
-// Place your definition and hook methods here:
-//
-//    /**
-//     * @Given /^I have done something with "([^"]*)"$/
-//     */
-//    public function iHaveDoneSomethingWith($argument)
-//    {
-//        doSomethingWith($argument);
-//    }
-//
+
+  /**
+   * Override MinkContext::fixStepArgument().
+   *
+   * Make it possible to use [random].
+   * If you want to use the previous random value [random:1].
+   */
+  protected function fixStepArgument($argument) {
+    $argument = str_replace('\\"', '"', $argument);
+
+    // Token replace the argument.
+    static $random = array();
+    for ($start = 0; ($start = strpos($argument, '[', $start)) !== FALSE; ) {
+      $end = strpos($argument, ']', $start);
+      if ($end === FALSE) {
+        break;
+      }
+      $random_generator = new Random;
+      $name = substr($argument, $start + 1, $end - $start - 1);
+      if ($name == 'random') {
+        $this->vars[$name] = $random_generator->name(8);
+        $random[] = $this->vars[$name];
+      }
+      // In order to test previous random values stored in the form,
+      // suppport random:n, where n is the number or random's ago
+      // to use, i.e., random:1 is the previous random value.
+      elseif (substr($name, 0, 7) == 'random:') {
+        $num = substr($name, 7);
+        if (is_numeric($num) && $num <= count($random)) {
+          $this->vars[$name] = $random[count($random) - $num];
+        }
+      }
+      if (isset($this->vars[$name])) {
+        $argument = substr_replace($argument, $this->vars[$name], $start, $end - $start + 1);
+        $start += strlen($this->vars[$name]);
+      }
+      else {
+        $start = $end + 1;
+      }
+    }
+
+    return $argument;
+  }
 }
---
+++ tests/behat/features/panopoly.feature
@@ -0,0 +1,28 @@
+Feature: Add landing page
+  In order to create a fully customizable page
+  As a site administrator
+  I need to be able to create a landing page
+
+  # @javascript is needed for the machine name
+  @api @javascript
+  Scenario: Add a landing page
+    Given I am logged in as a user with the "administrator" role
+    When I visit "/node/add/landing_page"
+    And I fill in the following:
+      | Title | Testing [random] landing page title |
+      | URL   | lp-[random:1]             |
+    And I press "Create Page"
+    Then the "h1" element should contain "Testing [random:1] landing page title"
+    When I customize this page with the Panels IPE
+    And I click "Add new pane"
+    And I click "Add text"
+    Then I should see "Configure new Add text"
+    When I fill in the following:
+      | Title   | Text widget title       |
+      | Editor  | plain_text              |
+      | Text    | Testing text body field |
+    And I press "edit-return"
+    And I press "Save"
+    And I wait for the Panels IPE to deactivate
+    Then I should see "Text widget title"
+    And I should see "Testing text body field"
