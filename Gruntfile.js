5d38dc4b1adb4acea60e0ddee08d659e15f62caf
lsolesen <lars@intraface.dk>
2014-10-29 08:16:02 +0000

    Improved phantomas and added wraith

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,13 +6,43 @@ module.exports = function(grunt) {
     phantomas: {
       default: {
         options: {
-          url: 'http://anvil-first.codio.io:3000'
+          numberOfRuns : 10,
+          url: 'http://anvil-first.codio.io:3000',
+          assertions : {
+            'assetsWithQueryString' : 3,
+            'biggestLatency'        : 1400,
+            'bodyHTMLSize'          : 10500,
+            'commentsSize'          : 55,
+            'consoleMessages'       : 0,
+            'hiddenContentSize'     : 65,
+            'jsErrors'              : 0,
+            'gzipRequests'          : 8,
+            'medianResponse'        : 400,
+            'nodesWithInlineCSS'    : 0,
+            'requests'              : 30,
+            'timeToFirstImage'      : 1100,
+            'DOMelementsCount'      : 200,
+            'DOMqueries'            : 10
+          },
+          options : {
+            'timeout' : 30
+          }
+        }
+      }
+    },
+    // End grunt-phantomas config
+    wraith: {
+      command: 'wraith capture config',
+      options: {
+        stdout: true,
+        execOptions: {
+          cwd: '<%= config.wraith %>'
         }
       }
     }
-    // End grunt-phantomas config
   });
 
   grunt.loadNpmTasks('grunt-phantomas');
   grunt.registerTask('default', ['phantomas']);
+  grunt.registerTask('wraith', ['wraith']);
 };
